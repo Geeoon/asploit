8a6204dc99bc84ca505b00e19045d98e292d46aa
Geeoon Chung <geeoon13@gmail.com>
2024-03-05 16:40:40 -0800

    Environment variables

--- src/main.py
+++ src/main.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-# @brief This python script will exploit a custom backdoor placed in on a website endpoint.
-
+# @brief This python script will exploit a custom backdoor placed in on a
+#       website endpoint.
+import os
 import traceback
 
 # @brief Exception raised to exit program.
 class ExitException(Exception):
-    pass
-
-# @brief Exception raised for non-existant commands.
-class NoCommand(Exception):
     pass
 
 # @brief Exception raised for command errors. Must be raised with a message.
@@ -25,49 +22,122 @@ class CommandProcessor():
     """
     def __init__(self):
         self.commands = {
-            "help": { "method": self.__help, "description": "Display the help screen" },
-            "exit": { "method": self.__exit, "description": "Exit the program" },
+            "help": { 
+                "method": self.__help,
+                "description": "Display the help screen."
+            },
+            "exit": {
+                "method": self.__exit,
+                "description": "Exit the program."
+            },
             "exploit": { 
                 "method": self.__exploit,
-                "description": "Start the exploit, given the preconditions are met."
+                "description": ("Start the exploit, "
+                    "given the preconditions are met.")
+            },
+            "clear": {
+                "method": self.__clear,
+                "description": "Clear the screen."
+            },
+            "set": {
+                "method": self.__set,
+                "description": "Set or display exploit variables."
             }
         }
-        self.target = ""
-        self.target_type = "ASP"
+
+        # exploit variables
+        self.variables = {
+            "TARGET": "",
+            "TARGET_TYPE": "ASP"
+        }
 
     """
-    @brief Simple help command that prints options and their usage.
-    @param options Not used.
+    @brief Help command that prints commands and their usage.
+    @param options The specific command to display help for.
     """
     def __help(self, options: str):
-        print("Help:")
+        # if no command is specified
+        if len(options) == 0:
+            for command in self.commands:
+                print((f"    {'{0: <10}'.format(command)}"
+                       f" {self.commands[command]['description']}"))
+        else:  # look up specific command
+            if options in self.commands:
+                print(f"    {options}: {self.commands[options]['description']}")
+            else:  # command does not exist
+                raise CommandException(f"    '{options}' command not found.")
 
     """
-    @brief Simple exit command that throws an ExitException.
+    @brief Command to exit the program.
+    @throw ExitException to exit the program.
     @param options Not used.
     """
     def __exit(self, options: str):
         raise ExitException()
     
     """
-    @brief Command to exploit the target.
+    @brief Command to clear the screen.
     @param options Not used.
-    @pre the TARGET field has been set and is a backdoored endpoint.
-    @pre the TARGET_TYPE field has been set and is a valid TARGET_TYPE.
-    @throw CommandException if the precondtions are not met.
     """
     def __exploit(self, options: str):
         print("Attempting to exploit target at ")
+
+    """
+    @brief Command to set exploit variables.
+    @param the variable to be set and the value, if any.
+    @throw CommandException if the variable does not exist, or if the vlaue is
+            invalid.
+    """
+    def __set(self, options: str):
+        if os.name == 'nt':  # Windows
+            os.system("cls")
+        else:  # POSIX
+            os.system("clear")
+
+    """
+    @brief Command to set exploit variables.
+    @param the variable to be set and the value, if any.
+    @throw CommandException if the variable does not exist, or if the vlaue is
+            invalid.
+    """
+    def __clear(self, options: str):
+        if os.name == 'nt':  # Windows
+            os.system("cls")
+        else:  # POSIX
+            os.system("clear")
+
+    """
+    @brief Command to set and display exploit variables.
+    @param the variable to be set and the value, if any.
+    @throw CommandException if the variable does not exist, or if the value is
+            invalid.
+    """
+    def __set(self, options: str):
+        if len(options) == 0:  # list all variables
+            print(f"{'{0: <20}'.format(f'VARIABLE')} VALUE")
+            for var in self.variables:
+                print(f"{'{0: <20}'.format(var)} {self.variables[var]}")
+        else:
+            split = options.split(" ", 1)
+            var = split[0]
+            if var in self.variables:
+                value = ""
+                if len(split) > 1:
+                    value = split[1]
+                self.variables[var] = value
+                print(f"'{var}' set to '{value}'")
+            else:
+                raise CommandException(f"'{var}' is not a variable.")
 
     """
     @brief Processes a command string.
     @param command A string representing the whole command to be processed.
     @pre \p command is a valid command to be ran.
     @pre \p command has its preconditions met. 
-    @throw NoCommand if the command doesn't exist.
+    @throw CommandException if the command doesn't exist.
     """
     def process_command(self, command: str):
-        split = command.split(" ", 2)
+        split = command.split(" ", 1)
         base = split[0]
 
         options = ""
@@ -75,9 +145,10 @@ class CommandProcessor():
             options = split[1]
         
         if base in self.commands:
-            self.commands[base](options)
+            self.commands[base]["method"](options)
         else:
-            raise NoCommand()
+            raise CommandException(
+                f"'{command.split(' ', 1)[0]}' command not found.")
 
 if __name__ == "__main__":
     processor = CommandProcessor()
@@ -88,8 +159,8 @@ if __name__ == "__main__":
             command = input("> ")
             try:
                 processor.process_command(command)
-            except NoCommand:
-                print(f"Couldn't find '{command.split(' ', 2)[0]}' command.")
+            except CommandException as e:
+                print(str(e))
     except ExitException:
         print("Exiting...")
     except KeyboardInterrupt:
