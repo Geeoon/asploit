c45f9d59b9f85fb057902bb07c4715e92cad7076
Geeoon Chung <geeoon13@gmail.com>
2024-03-20 12:05:36 -0700

    botnet remove

--- src/BotnetCommandProcessor.py
+++ src/BotnetCommandProcessor.py
@@ -12,20 +12,27 @@ class BotnetCommandProcessor(CommandProcessor):
         self.targets = []
         super().__init__()
         self.commands["add"] = {
-                "method": self.__add,
-                "description": "Add a target to the list of targets.",
-                "usage": ("add name host path type method header\n"
-                          "    name: the name of the target.\n"
-                          "    host: the hostname of the target.\n"
-                          "    path: the path of the backdoor on the target.\n"
-                          "    type: the type of backdoor on the target.\n"
-                          "    method: the HTTP method for the backdoor.\n"
-                          "    header: the HTTP header to communicate over.")
-            }
+            "method": self.__add,
+            "description": "Add a target to the list of targets.",
+            "usage": ("add name host path type method header\n"
+                        "    name: the name of the target.\n"
+                        "    host: the hostname of the target.\n"
+                        "    path: the path of the backdoor on the target.\n"
+                        "    type: the type of backdoor on the target.\n"
+                        "    method: the HTTP method for the backdoor.\n"
+                        "    header: the HTTP header to communicate over.")
+        }
         self.commands["list"] = {
-                "method": self.__list,
-                "description": "List all the targets.",
-                "usage": ("list")
+            "method": self.__list,
+            "description": "List all the targets.",
+            "usage": "list"
+        }
+        self.commands["remove"] = {
+            "method": self.__remove,
+            "description": "Remove a target from the botnet.",
+            "usage": ("remove names ...\n"
+                      "    names: a space seperated list of the names of the"
+                      " targets to be removed.")
         }
         
     """
@@ -50,6 +57,7 @@ class BotnetCommandProcessor(CommandProcessor):
 
     """
     @brief List all of the hosts
+    @throw CommandException if there are not targets.
     """
     def __list(self, options: str):
         if len(self.targets) == 0:
@@ -65,6 +73,24 @@ class BotnetCommandProcessor(CommandProcessor):
             print(f"{target['host']}{target['path']}")
             print(target["type"])
             print(target["header"])
+        print(f"\nTotal: {len(self.targets)}")
+
+    """
+    @brief Remove targets from the list.
+    @param options a list of targets to be removed (space seperated).
+    @throw CommandException if there are no arguments.
+    """
+    def __remove(self, options: str):
+        if len(options) == 0:
+            raise CommandException("Nothing to be removed.")
+
+        for name in options.split(" "):
+            target = next((t for t in self.targets if t["name"] == name), None)
+            if target:
+                print(f"Removing '{name}'.")
+                self.targets.remove(target)
+            else:
+                print(f"'{name}' not found in targets.")
 
     """
     @brief See base class for details.
